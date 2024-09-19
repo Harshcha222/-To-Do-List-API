44e33c4003f802a233209271e5126b58b1de8267
Harshcha222 <rchaturvedi087@gmail.com>
2024-09-19 11:38:13 +0530

    ..

--- todo_project/core/urls.py
+++ todo_project/core/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# core/urls.py
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from .views import home  # Import the home view
 
 urlpatterns = [
+    path('', home),  # Handle the root URL
     path('admin/', admin.site.urls),
     path('api/', include('todo_app.urls')),
 ]
+
